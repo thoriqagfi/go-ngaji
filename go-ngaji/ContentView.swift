f1af55eba20f2e929534f0617806d00cec0d94ea
Agfi <thoriq.aghfi60@gmail.com>
2024-03-28 15:50:11 +0700

    Initial Commit

---
+++ go-ngaji/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  go-ngaji
+//
+//  Created by Agfi on 28/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ go-ngaji/go_ngajiApp.swift
@@ -0,0 +1,17 @@
+//
+//  go_ngajiApp.swift
+//  go-ngaji
+//
+//  Created by Agfi on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct go_ngajiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
